91adea9b0effc588f4dab1515b2e4337e4fe7c9a
Mayank <mayankguptacse029@gmail.com>
2021-06-23 17:55:40 +0530

    balance api completed

--- MHG/controllers/userMasterController.js
+++ MHG/controllers/userMasterController.js
@@ -1,5 +1,8 @@
 const UserMaster = require('../models/userMaster');
 
+// For mer_id validation:
+const Merchants = require('../models/merchant');
+
 
 // userMaster_get:
 
@@ -14,31 +17,95 @@ const master_get = ((req, res) => {
 
 // balance_get:
 
-const balance_get = ((req, res) =>{
+const balance_get = (req, res) =>{
     
     // console.log(req.headers.merchant_id);
-    
+    const userId = req.params.userId
+    console.log(userId);
     // Step1 : Authenticate the merchant_id:
     // Step2 : Check if the userId is present in the db.
     // Step3 : If (present) -----> return the balance.
     // Step4 : If (absent)  -----> Make a new account with given userId and balance 0.
 
-    const userId  = req.params.userId;
-    UserMaster.find()
-    .select(user_id).exec()
-    .then((result)=> res.status(200).json(
-            {
-                balance:result.filter(res=>{
-                    return{
-                        // _id:res._id,
-                        name:res.name
-                    }
-                })
-            }
-        )) 
-    .catch(()=> res.status(400).json({message:'No users found'}))
+    // step:1 
 
-});
+
+    const mer_id = req.headers.merchant_id;         //encoded
+    const decodedData = Buffer.from(mer_id, 'base64').toString('ascii') // decoded
+
+
+    // decodedData is something like this: Basic 604f00e19d5b6e44a30cca77:604f00e19d5b6e44a30cca77
+    // extacting mid and mid_secret from it.
+
+    let mid = "", mid_secret = "";
+    let i;
+    for (i = 6; i < decodedData.length; i++) {
+        if (decodedData[i] == ':')
+            break;
+        mid += decodedData[i];
+    }
+
+    for (let j = i + 1; j < decodedData.length; j++) {
+        mid_secret += decodedData[j];
+    }
+
+    // console.log(decodedData);
+
+     // check if mer_id constains Basic or not:
+     const basic = decodedData.includes('Basic');
+
+     // if mid is not provided.
+     if (!mer_id) return res.status(404).send('No Token');
+
+     
+
+
+     // search in database if mid exist or not 
+    // if it exists then execute step(2,3,4)
+
+
+    Merchants.find({ mid: mid, mid_secret: mid_secret }, (err, result) => {
+        if (err) res.status(400).json({ message: 'No such Merchant' });
+        else if (!result.length || basic === false) res.status(403).json({ messsage: 'No such Merchant' })
+        else
+            {
+
+    // step: 2,3,4
+
+    UserMaster.findOne({user_id: userId})  
+    .then (user => {
+            if(user)
+                res.status(200).json
+                        ({
+                            code:2001,
+                            messsage:'success',
+                            status:'SUCCESS',
+                            balance: user.price_point_value
+                        })
+            else
+                {
+                    const user =  new UserMaster({
+                        user_id:userId,
+                        merchant_id:mid,
+                        price_point_value:0
+                    })
+                    user.save()
+                    .then(user =>{
+                        res.status(200).json
+                        ({
+                            code:2001,
+                            messsage:'success',
+                            status:'SUCCESS',
+                            balance: user.price_point_value
+                        })
+                    })
+                    .catch(err => {res.status(404).json({err:err.message})})
+                }
+        })
+    .catch(err => res.status(404).json({ messsage: err.message || err.toString()}))
+    }
+    })
+};
 
 
 module.exports = { master_get,balance_get};
--- MHG/models/userMaster.js
+++ MHG/models/userMaster.js
@@ -3,8 +3,13 @@ const Schema = mongoose.Schema;
 
 const masterSchema = new Schema(
     {
-
-    }
+        name:String,
+        email:String,
+        mobile:String,
+        price_point_value:Number,
+        user_id:String, 
+        merchant_id:String
+    },{timestamps: true}
 );
 
 const UserMaster = mongoose.model('User_Master', masterSchema);
